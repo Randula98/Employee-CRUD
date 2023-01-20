39e9864d1d0ef72e4469da9239ca2b59853be444
Randula Mawaththta <randula98@gmail.com>
2023-01-20 16:06:02 +0530

    Navbar Implemented

---
+++ Frontend/src/routes/app-routes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import NavBar from "../components/navbar";
+
+// import {
+
+// } from "../pages";
+
+function App() {
+    return (
+        <>
+            <Router>
+                <NavBar />
+                    <Routes>
+
+                    </Routes>
+                
+            </Router>
+        </>
+    )
+}
+
+export default App;
