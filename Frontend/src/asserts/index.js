ad1e479ff25a913bded38b212d61e989543069ce
Randula Mawaththta <randula98@gmail.com>
2023-01-21 11:52:43 +0530

    NavBar Link Updated

--- Frontend/src/asserts/index.js
+++ Frontend/src/asserts/index.js
@@ -1,6 +1,6 @@
 //images
-const registerImage = "https://firebasestorage.googleapis.com/v0/b/employee-crud-39d26.appspot.com/o/asserts%2FMobile-login.jpg?alt=media&token=f4df4fec-50e1-4264-aec2-0e2d091b036c";
-const loginImage = "https://firebasestorage.googleapis.com/v0/b/employee-crud-39d26.appspot.com/o/asserts%2Fdraw2.webp?alt=media&token=3a850439-8401-431f-9b5f-fe9e7a6d5e45";
+const registerImage = "https://firebasestorage.googleapis.com/v0/b/employee-crud-39d26.appspot.com/o/asserts%2FMesa%20de%20trabajo%201.png?alt=media&token=0f9e61ac-4082-4525-b401-19aa88765ad0";
+const loginImage = "https://firebasestorage.googleapis.com/v0/b/employee-crud-39d26.appspot.com/o/asserts%2Fillustration-people-login.png?alt=media&token=af63ab7a-ad5e-4041-a96b-786d305e9f87";
 const homeLoginImage = "https://firebasestorage.googleapis.com/v0/b/employee-crud-39d26.appspot.com/o/asserts%2F4957136.jpg?alt=media&token=f160a402-4514-45fc-9438-2f89c8f2d4a1";
 const homeRegisterImage = "https://firebasestorage.googleapis.com/v0/b/employee-crud-39d26.appspot.com/o/asserts%2F6368592.jpg?alt=media&token=cb81a178-c667-4492-b93a-662ea7ce3cea";
 
